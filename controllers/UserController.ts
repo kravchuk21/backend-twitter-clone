f8a95ee706714d0be3fc4b05e02a2da299ffe866
kravchuk21 <krauchukvlad@gmail.com>
2020-09-27 22:22:25 +0300

    Урок №7

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
 import { validationResult } from 'express-validator';
-import { UserModel, UserModelInterface } from '../models/UserModel';
+import { UserModel, UserModelInterface, UserModelDocumentInterface } from '../models/UserModel';
 import { generateMD5 } from '../utils/generateHash';
 import { sendEmail } from '../utils/sendEmail';
+
+const isValidObjectId = mongoose.Types.ObjectId.isValid;
 
 class UserController {
   async index(_: any, res: express.Response): Promise<void> {
@@ -12,6 +17,34 @@ class UserController {
       res.json({
         status: 'success',
         data: users,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
+  }
+
+  async show(req: any, res: express.Response): Promise<void> {
+    try {
+      const userId = req.params.id;
+
+      if (!isValidObjectId(userId)) {
+        res.status(400).send();
+        return;
+      }
+
+      const user = await UserModel.findById(userId).exec();
+
+      if (!user) {
+        res.status(404).send();
+        return;
+      }
+
+      res.json({
+        status: 'success',
+        data: user,
       });
     } catch (error) {
       res.status(500).json({
@@ -33,7 +66,7 @@ class UserController {
         email: req.body.email,
         username: req.body.username,
         fullname: req.body.fullname,
-        password: req.body.password,
+        password: generateMD5(req.body.password + process.env.SECRET_KEY),
         confirmHash: generateMD5(process.env.SECRET_KEY || Math.random().toString()),
       };
 
@@ -91,6 +124,41 @@ class UserController {
       } else {
         res.status(404).json({ status: 'error', message: 'Пользователь не найден' });
       }
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
+  }
+
+  async afterLogin(req: express.Request, res: express.Response): Promise<void> {
+    try {
+      const user = req.user ? (req.user as UserModelDocumentInterface).toJSON() : undefined;
+      res.json({
+        status: 'success',
+        data: {
+          ...user,
+          token: jwt.sign({ data: req.user }, process.env.SECRET_KEY || '123', {
+            expiresIn: '30d',
+          }),
+        },
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
+  }
+
+  async getUserInfo(req: express.Request, res: express.Response): Promise<void> {
+    try {
+      const user = req.user ? (req.user as UserModelDocumentInterface).toJSON() : undefined;
+      res.json({
+        status: 'success',
+        data: user,
+      });
     } catch (error) {
       res.status(500).json({
         status: 'error',
--- server.ts
+++ server.ts
@@ -6,21 +6,25 @@ import './core/db';
 import express from 'express';
 import { UserCtrl } from './controllers/UserController';
 import { registerValidations } from './validations/register';
+import { passport } from './core/passport';
 
 const app = express();
 
 app.use(express.json());
+app.use(passport.initialize());
 
 /*
   TODO:
-  1. Скрыть поля confirmHash и password при получении инфы о пользователе
   2. Сделать авторизацию через JWT + Passport
   3. Сделать возможность добавлять твиты через авторизованного пользователя
 */
 
 app.get('/users', UserCtrl.index);
-app.post('/users', registerValidations, UserCtrl.create);
-app.get('/users/verify', registerValidations, UserCtrl.verify);
+app.get('/users/me', passport.authenticate('jwt'), UserCtrl.getUserInfo);
+app.get('/users/:id', UserCtrl.show);
+app.get('/auth/verify', registerValidations, UserCtrl.verify);
+app.post('/auth/register', registerValidations, UserCtrl.create);
+app.post('/auth/login', passport.authenticate('local'), UserCtrl.afterLogin);
 // app.patch('/users', UserCtrl.update);
 // app.delete('/users', UserCtrl.delete);
 
