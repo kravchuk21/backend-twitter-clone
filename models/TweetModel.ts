d4a6e8ab0e02c2a4765157177072f94f2849a34b
kravchuk21 <krauchukvlad@gmail.com>
2021-01-04 22:40:23 +0300

    Урок №15

--- models/TweetModel.ts
+++ models/TweetModel.ts
@@ -5,23 +5,32 @@ export interface TweetModelInterface {
   _id?: string;
   text: string;
   user: UserModelDocumentInterface;
+  images?: string[];
 }
 
 export type TweetModelDocumentInterface = TweetModelInterface & Document;
 
-const TweetSchema = new Schema<TweetModelInterface>({
-  text: {
-    required: true,
-    type: String,
-    maxlength: 280,
+const TweetSchema = new Schema<TweetModelInterface>(
+  {
+    text: {
+      required: true,
+      type: String,
+      maxlength: 280,
+    },
+    user: {
+      required: true,
+      ref: 'User',
+      type: Schema.Types.ObjectId,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
   },
-  user: {
-    required: true,
-    ref: 'User',
-    type: Schema.Types.ObjectId,
+  {
+    timestamps: true,
   },
-}, {
-  timestamps: true
-});
+);
 
 export const TweetModel = model<TweetModelDocumentInterface>('Tweet', TweetSchema);
